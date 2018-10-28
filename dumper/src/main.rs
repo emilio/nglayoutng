59237b14e0745a0749c2f6d8c96e7b660a19d11b
Emilio Cobos Álvarez <emilio@crisal.io>
2018-10-28 05:34:08 +0100

    More progress towards having a working dumper.

--- dumper/src/main.rs
+++ dumper/src/main.rs
@@ -1,2 +1,27 @@
+#[macro_use]
+extern crate clap;
+extern crate nglayoutng;
+
+use nglayoutng::layout_tree::builder::LayoutTreeBuilder;
+use std::fs::File;
+
 fn main() {
+    use clap::{AppSettings, SubCommand};
+
+    let args = app_from_crate!()
+        .subcommand(
+            SubCommand::with_name("dump")
+                .about("Dumps a layout tree from an HTML document")
+                .arg_from_usage("<input>  'The document to build the tree for'")
+        )
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .get_matches();
+
+    if let Some(args) = args.subcommand_matches("dump") {
+        let input = args.value_of("input").unwrap();
+        let mut file = File::open(input).expect("Couldn't open input file");
+
+        let _builder = LayoutTreeBuilder::new(&mut file)
+            .expect("Failed to parse input file?");
+    }
 }
